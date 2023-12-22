5fa4c505fadb1ec3298304e901feb43e24d3b1c9
Whilliah <shirao.shotaro@gmail.com>
2023-12-22 22:26:31 +0900

    XformOpWidget を更新

--- simpleEditor/edit/PropertyEdit/XformOpWidget.py
+++ simpleEditor/edit/PropertyEdit/XformOpWidget.py
@@ -1,22 +1,11 @@
-from pxr import UsdGeom
-from PySide2.QtCore import (
-    QAbstractItemModel,
-    Qt,
-    QModelIndex,
-    QSize,
-    QStringListModel,
-)
-from PySide2.QtGui import (
-    QIcon,
-    QBrush,
-    QFont,
-)
+from pxr import UsdGeom, Sdf
+from PySide2.QtCore import Qt
 from PySide2.QtWidgets import (
-    QAction,
-    QHBoxLayout,
-    QListView,
-    QMenu,
+    QGridLayout,
+    QLabel,
+    QFrame,
     QPushButton,
+    QToolButton,
     QVBoxLayout,
     QWidget,
 )
@@ -27,45 +16,98 @@ from .AddXformOpDialog import AddXformOpDialog
 class XformOpWidget(QWidget):
     def __init__(self, attr, currentTime, parent):
         super().__init__(parent)
-        self._attr = attr
-        self._currentTime = currentTime
-        self._model = QStringListModel()
-        self._layout = QHBoxLayout()
-        self._listView = QListView(self)
-        self._listView.setModel(self._model)
-        self._cmdLayout = QVBoxLayout()
-        self._addButton = QPushButton(self)
-        self._forwardButton = QPushButton(self)
-        self._backButton = QPushButton(self)
-        self._removeButton = QPushButton(self)
-        self._addButton.setText("+")
-        self._forwardButton.setText("↑")
-        self._backButton.setText("↓")
-        self._removeButton.setText("-")
-        self._cmdLayout.addWidget(self._addButton)
-        self._cmdLayout.addStretch()
-        self._cmdLayout.addWidget(self._forwardButton)
-        self._cmdLayout.addWidget(self._backButton)
-        self._cmdLayout.addStretch()
-        self._cmdLayout.addWidget(self._removeButton)
-        self._cmdLayout.setContentsMargins(0, 0, 0, 0)
-        self._layout.addWidget(self._listView)
-        self._layout.addLayout(self._cmdLayout)
-        self._layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(self._layout)
+        self.setStyleSheet(
+            """
+            QFrame#xopsWidget { border: 1px solid #888888; border-radius: 2px; }
+            QToolButton { border: none; border-radius: 0; margin: 0; padding: 0; }
+        """
+        )
         self.setContentsMargins(0, 0, 0, 0)
-
-        # signal
+        self.setLayout(QVBoxLayout())
+        self.layout().setMargin(0)
+        self._rootWidget = None
+        self._layout = None
+        self._addButton = QPushButton()
+        self._addButton.setText("+")
         self._addButton.clicked.connect(self._addXformOp)
-
-        self.sync(currentTime)
+        self._currentTime = currentTime
+        self._attr = attr
+        self._xform = UsdGeom.Xformable(self._attr.GetPrim())
+        self._ops = list()
+        self._opsDirty = True
+        self.sync(self._currentTime)
 
     def sync(self, currentTime):
+        from simpleEditor.edit.AttributeWidget import AttributeWidget
+
+        if self._opsDirty:
+            self.layout().takeAt(0)
+            if self._rootWidget:
+                self._rootWidget.deleteLater()
+            self._rootWidget = QFrame()
+            self._rootWidget.setObjectName("xopsWidget")
+            self._layout = QGridLayout()
+            self._layout.setHorizontalSpacing(4)
+            self._layout.setVerticalSpacing(4)
+            self._rootWidget.setLayout(self._layout)
+            self._ops.clear()
+            ops = self._xform.GetOrderedXformOps()
+            rowCount = len(ops)
+            for row, op in enumerate(ops):
+                opAttr = op.GetAttr()
+                label = QLabel()
+                labelText = op.GetOpName().split(":", 2)[1]
+                if op.IsInverseOp():
+                    labelText = "[INV]" + labelText
+                label.setText(labelText)
+                fwdButton = QToolButton(self._rootWidget)
+                fwdButton.setText("↑")
+                fwdButton.setVisible(row > 0)
+                fwdButton.clicked.connect(lambda *, r=row, f=-1: self._moveOp(r, f))
+                backButton = QToolButton(self._rootWidget)
+                backButton.setText("↓")
+                backButton.setVisible(row < (rowCount - 1))
+                backButton.clicked.connect(lambda *, r=row, f=1: self._moveOp(r, f))
+                removeButton = QToolButton(self._rootWidget)
+                removeButton.setText("×")
+                removeButton.clicked.connect(lambda *, r=row: self._removeOp(r))
+                opWidget = AttributeWidget(opAttr, currentTime, self._rootWidget)
+                if hasattr(opWidget.getWidget(), "valueChanged"):
+                    opWidget.getWidget().valueChanged.connect(self._resync)
+                self._layout.addWidget(fwdButton, row, 0)
+                self._layout.addWidget(backButton, row, 1)
+                self._layout.addWidget(label, row, 2, Qt.AlignVCenter | Qt.AlignRight)
+                self._layout.addWidget(opWidget, row, 3)
+                self._layout.addWidget(removeButton, row, 4)
+                self._ops.append((op, opWidget, fwdButton, backButton, label, removeButton))
+            self._layout.addWidget(self._addButton, rowCount, 2)
+            self._layout.setColumnStretch(3, 1)
+            self.layout().addWidget(self._rootWidget)
+            self._opsDirty = False
+        else:
+            for op in self._ops:
+                op[1].getWidget().sync(currentTime)
         self._currentTime = currentTime
-        with SignalBlocker(self):
-            value = self._attr.Get(currentTime)
-            self._model.setStringList(value if value else list())
 
     def _addXformOp(self):
-        if AddXformOpDialog.addXformOp(self, self._attr.GetPrim()):
+        if AddXformOpDialog.addXformOp(self._addButton, self._xform):
+            self._opsDirty = True
             self.sync(self._currentTime)
+
+    def _moveOp(self, row, f):
+        ops = list(self._xform.GetXformOpOrderAttr().Get())
+        dst = row + f
+        ops[row], ops[dst] = ops[dst], ops[row]
+        self._attr.Set(ops)
+        self._opsDirty = True
+        self.sync(self._currentTime)
+
+    def _removeOp(self, row):
+        ops = list(self._attr.Get())
+        del ops[row]
+        self._attr.Set(ops)
+        self._opsDirty = True
+        self.sync(self._currentTime)
+
+    def _resync(self):
+        self.sync(self._currentTime)
